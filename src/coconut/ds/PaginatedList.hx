437037ee67f99cd6eccf9560afa1a1b298d3d00a
Kevin Leung <kevinresol@gmail.com>
2017-08-30 14:08:49 +0800

    Add maxPage and cache to PagedList

---
+++ src/coconut/ds/PaginatedList.hx
@@ -0,0 +1,31 @@
+package coconut.ds;
+
+import tink.pure.*;
+import coconut.data.*;
+
+using tink.CoreApi;
+
+class PaginatedList<T> implements Model {
+	@:editable var page:Int = @byDefault 0; // zero-indexed
+	@:constant var perPage:Int;
+	@:constant var loader:{
+		function getMaxPage(perPage:Int):Promise<Int>;
+		function getData(page:Int, perPage:Int):Promise<List<T>>;
+	}
+	@:loaded var maxPage:Int = loader.getMaxPage(perPage); // zero-indexed
+	@:loaded var data:List<T> = 
+		if(cache.exists(page))
+			cache.get(page);
+		else {
+			var loaded = loader.getData(page, perPage);
+			loaded.handle(function(o) switch o {
+				case Success(data): cache = cache.with(page, data);
+				case Failure(_):
+			});
+			loaded;
+		}
+	@:editable private var cache:Mapping<Int, List<T>> = new Mapping();
+	
+	// let's assume on the remote side the data is immutable as well
+	// so we don't need to provide mechanisms to reset maxPage and cache
+}
