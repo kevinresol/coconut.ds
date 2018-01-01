8d0db8a81d6385479cb45f549304cffc03b04f6c
Kevin Leung <kevinresol@gmail.com>
2018-01-01 12:07:06 +0800

    Allow hard refresh

--- src/coconut/ds/Cache.hx
+++ src/coconut/ds/Cache.hx
@@ -4,16 +4,30 @@ using tink.CoreApi;
 
 class Cache<T> implements coconut.data.Model {
 	@:constant var load:Void->Promise<T>;
-	@:editable private var cache:Option<T> = @:byDefault None;
-	@:loaded var data:T = 
+	@:observable private var revision:Int = @byDefault 0;
+	@:observable private var cache:Option<T> = @byDefault None;
+	@:loaded var data:T = {
+		trace('load data');
+		revision;
 		switch cache {
 			case Some(v): v;
 			case None: load();
 		} 
+	}
 	
-	public function setCache(v:T)
-		cache = Some(v);
+	@:transition
+	function refresh() {
+		return {
+			revision: revision + 1,
+			cache: None,
+		}
+	}
 	
-	public function clearCache()
-		cache = None;
+	@:transition
+	function setCache(v:T)
+		return {cache: Some(v)};
+	
+	@:transition
+	function clearCache()
+		return {cache: None};
 }
