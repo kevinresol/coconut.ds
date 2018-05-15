4fa143e0096f7c6db3b0f9ebc9876dca18ea2f94
Kevin Leung <kevinresol@gmail.com>
2018-05-15 10:59:16 +0800

    Simplify the API abit

--- src/coconut/ds/PaginatedList.hx
+++ src/coconut/ds/PaginatedList.hx
@@ -3,51 +3,28 @@ package coconut.ds;
 import tink.pure.*;
 import coconut.data.*;
 
+using tink.state.Promised;
 using tink.CoreApi;
 
+typedef Envelope<T> = {
+	var total(default, never):Int;
+	var data(default, never):List<T>;
+}
+
 class PaginatedList<T> implements Model {
-	@:observable var page:Int = @byDefault -1; // zero-indexed
-	@:constant var perPage:Int;
-	@:constant var loader:{
-		function load(page:Int, perPage:Int):Promise<{total:Int, data:List<T>}>;
-	}
-	@:observable var total:Int = @byDefault -1;
-	@:computed var maxPage:Int = total == -1 ? -1 : Math.ceil(total / perPage) - 1;
-	@:observable var data:List<T> = @byDefault null;
 	
-		// if(cache.exists(page))
-		// 	cache.get(page);
-		// else {
-		// 	var loaded = loader.getData(page, perPage);
-		// 	var current = page; // page may be modified when loading
-		// 	loaded.handle(function(o) switch o {
-		// 		case Success(data): cache = cache.with(current, data);
-		// 		case Failure(_):
-		// 	});
-		// 	loaded;
-		// }
-	// @:editable private var cache:Mapping<Int, List<T>> = new Mapping();
+	@:editable var page:Int = @byDefault 0;
+	@:constant var perPage:Int = @byDefault 10;
+	@:constant var load:Int->Int->Promise<Envelope<T>>; // page->perPage->result
+	@:loaded var current:Envelope<T> = load(page, perPage);
+	@:loaded var total:Int = current.next(function(e) return e.total);
+	@:loaded var maxPage:Int = total.next(function(v) return Math.floor(v / perPage));
+	@:loaded var data:List<T> = current.next(function(e) return e.data);
+	// @:constant var cache:ObservableMap<Int, Envelope> = new ObservableMap();
 	
-	@:transition
-	function nextPage()
-		return setPage(page + 1);
+	inline function nextPage()
+		page += 1;
 	
-	@:transition
-	function prevPage()
-		return setPage(page - 1);
-	
-	@:transition
-	function setPage(page:Int) {
-		if(isInTransition) return Promise.lift(new Error('Already loading'));
-		if(this.page == page) return @patch {};
-		return loader.load(page, perPage)
-			.next(function(o) return {
-				page: page,
-				total: o.total,
-				data: o.data,
-			});
-	}
-	
-	// let's assume on the remote side the data is immutable as well
-	// so we don't need to provide mechanisms to reset maxPage and cache
+	inline function prevPage()
+		page -= 1;
 }
