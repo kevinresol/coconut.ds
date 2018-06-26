ac1af91ef3c3d7ba8dfcd281e32c5811b77f64ee
Kevin Leung <kevinresol@gmail.com>
2018-06-26 15:01:45 +0800

    Put back isInTransition functionality

--- src/coconut/ds/InfiniteList.hx
+++ src/coconut/ds/InfiniteList.hx
@@ -2,13 +2,14 @@ package coconut.ds;
 
 import coconut.ds.cache.*;
 import coconut.data.*;
+import tink.state.*;
 using tink.CoreApi;
 
 @:forward
 abstract InfiniteList<T>(InfiniteListImpl<T>) from InfiniteListImpl<T> to InfiniteListImpl<T> {
-	public function new(init) {
-		this = new InfiniteListImpl<T>(init);
-		this.init();
+	public function new(initial) {
+		this = new InfiniteListImpl<T>(initial);
+		Observable.untracked(this.init);
 	}
 }
 
@@ -20,43 +21,54 @@ private class InfiniteListImpl<T> implements Model {
 	@:constant var cache:Cache<List<T>> = @byDefault new NoCache();
 	
 	@:editable private var cached:List<T> = null;
-	@:editable private var loaded:Option<List<T>> = None;
+	@:observable private var loaded:Option<List<T>> = None;
 	
 	@:computed var list:List<T> = loaded.or(cached);
 	@:computed var last:Option<T> = list.last();
 	
-	public function reset() {
+	@:transition
+	function reset() {
 		cached = null;
-		loaded = None;
+		return {
+			loaded: None,
+		}
 	}
 	
+	@:transition
 	function init() {
 		cache.get().handle(function(v) cached = v.orNull());
-		return refresh();
-	}
-	
-	public function refresh() {
 		return loadAfter(None, true);
 	}
 	
-	public function loadNext() {
+	@:transition
+	function refresh() {
+		return loadAfter(None, true);
+	}
+	
+	@:transition
+	function loadNext() {
 		return loadAfter(last);
 	}
 	
-	public function set(v) {
-		loaded = Some(v);
+	@:transition
+	function set(v) {
+		return {
+			loaded: Some(v),
+		}
 	}
 	
-	public function loadAfter(last, reset = false) {
+	function loadAfter(last, reset = false):Promise<coconut.data.Patch<InfiniteListImpl<T>>> {
+		// if(isInTransition) return new Error('Already refreshing');
 		return load(last, perPage)
-			.next(function(v) {
+			.next(function(v):coconut.data.Patch<InfiniteListImpl<T>> {
 				var updated = switch loaded {
 					case Some(list) if(!reset): concat(list, v);
 					case _: v;
 				};
 				cache.set(updated).eager();
-				loaded = Some(updated);
-				return Noise;
+				return {
+					loaded: Some(updated),
+				}
 			});
 	}
 }
