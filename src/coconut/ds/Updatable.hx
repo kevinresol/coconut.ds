c7f9c8e1701ef29cf98823f67cbe57eea7e7466c
Kevin Leung <kevinresol@gmail.com>
2019-06-08 10:11:03 +0800

    Pass current data to updater too

--- src/coconut/ds/Updatable.hx
+++ src/coconut/ds/Updatable.hx
@@ -1,10 +1,11 @@
 package coconut.ds;
 
 using tink.CoreApi;
+using tink.state.Promised;
 
 class Updatable<Content, Patch> implements coconut.data.Model {
 	@:constant var loader:Void->Promise<Content>;
-	@:constant var updater:Patch->Promise<Content>;
+	@:constant var updater:Content->Patch->Promise<Content>;
 	@:editable private var revision:Int = 0;
 	@:editable private var cache:Content = @byDefault null;
 	@:loaded var data:Content = {revision; cache != null ? cache : loader();}
@@ -14,8 +15,8 @@ class Updatable<Content, Patch> implements coconut.data.Model {
 		revision++;
 	}
 	
-	public function update(data) {
-		var ret = updater(data);
+	public function update(patch) {
+		var ret = observables.data.getNext(v -> v.toOption()).next(updater.bind(_, patch));
 		ret.handle(function(o) switch o {
 			case Success(v): refresh(v);
 			case Failure(e): // skip
